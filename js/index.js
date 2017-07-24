eba4d44ee3d7b81c52321eddb8edd78c1c8efd27
Flockguru <Plumlytics@Venuss-MacBook-Air.local>
2017-07-24 13:43:28 -0400

    update

--- js/index.js
+++ js/index.js
@@ -19,32 +19,6 @@ $(document).ready(function(){
   });
 
 
-  semantic.embed = {};
-
-  // ready event
-  semantic.embed.ready = function() {
-
-    // selector cache
-    var
-      // alias
-      $myfavesEmbed = $('.example').eq(0).find('iframe'),
-      handler
-    ;
-
-    // event handlers
-    handler = {
-
-    };
-
-    $('.embed.example .ui.embed')
-      .embed()
-    ;
-    $('.existing.code .embed').removeClass('embed').addClass('ui embed');
-
-
-  };
-
-
   // Set custom step
 
  pricings= [
