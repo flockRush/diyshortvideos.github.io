21f00adc047754a8b4bd3da94c4db42537f98c4f
Flockguru <Plumlytics@Venuss-MacBook-Air.local>
2017-07-24 14:32:43 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,60 +1,58 @@
 $(document).ready(function(){
-
-  $('.topnav.sidebar').first()
-   .sidebar('attach events', '.toggle.button')
-  ;
-  $('.toggle.button')
-   .removeClass('disabled')
-  ;
-
-
-  $("[data-popup]").each(function(_, item){
-    item = $(item);
-    item.popup({
-      popup: "#" + item.data("popup"),
-      exclusive: true,
-      hoverable: true,
-      lastResort: "bottom left"
+  $(".topnav.sidebar")
+    .first()
+    .sidebar("attach events", ".toggle.button");
+  $(".toggle.button")
+    .removeClass("disabled");
+  $(".existing.code .embed")
+    .removeClass("embed")
+    .addClass("ui embed");
+  $(".ui.embed")
+    .embed();
+  $("[data-popup]")
+    .each(function(_, item){
+      item = $(item);
+      item.popup({
+        popup: "#" + item.data("popup"),
+        exclusive: true,
+        hoverable: true,
+        lastResort: "bottom left"
+      });
     });
+
+  // set custom step
+  pricings = [
+    null,
+    "0",
+    "499",
+    "999",
+    "1499",
+    "2,499",
+    "4,499",
+    "8,999",
+  ]
+  audiences = [
+    null,
+    "0",
+    "100,000",
+    "1,000,000",
+    "2,000,000",
+    "3,000,000",
+    "5,000,000",
+    "10,000,000",
+  ]
+  $("#pricing-slider").range({
+    min: 1,
+    max: 7,
+    start: 3,
+    step: 1,
+    onChange: function(value){
+      $("#display-pricing").html(pricings[value]);
+      $("#display-audience").html(audiences[value]);
+    }
   });
-
-
-  // Set custom step
-
- pricings= [
- null,
- "0",
- "499",
- "999",
- "1499",
- "2,499",
- "4,499",
- "8,999",
- ]
- audiences= [
- null,
- "0",
- "100,000",
- "1,000,000",
- "2,000,000",
- "3,000,000",
- "5,000,000",
- "10,000,000",
- ]
- $('#pricing-slider').range({
-  min: 1,
-  max: 7,
-  start: 3,
-  step: 1,
-  onChange: function(value) {
-    $('#display-pricing').html(pricings[value]);
-    $('#display-audience').html(audiences[value]);
-  }
+  $(".overlay").visibility({
+    type: "fixed",
+    offset: 80
   });
-
-  $('.overlay').visibility({
-  type: 'fixed',
-  offset: 80
-});
-
 });
